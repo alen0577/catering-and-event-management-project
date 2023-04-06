366977488f6e7504a8235bfb7b83619b9eec61d9
Alen Antony <alenantony32@gmail.com>
2023-04-07 00:00:02 +0530

    modified

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('admin-home_page/', views.adminhome,name='adminhome'),
+    path('user-home_page/',views.userhome,name='userhome'),
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
 
 # Create your views here.
 
 def index(request):
     return render(request,'home/index.html')
+
+
+
+
+def adminhome(request):
+    return render(request,'manager/adminhome.html')
+
+
+def userhome(request):
+    return render(request,'user/userhome.html')
